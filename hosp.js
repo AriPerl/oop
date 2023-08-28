b17a6813b2186e53102ee2ebeb32b5ed50c42595
AriPerl <arie2001a@gmail.com>
2023-08-28 15:49:47 +0300

    first commit

--- hosp.js
+++ hosp.js
@@ -14,7 +14,9 @@ var __extends = (this && this.__extends) || (function () {
     };
 })();
 var Person = /** @class */ (function () {
-    function Person(fName, lName) {
+    function Person(fName, lName, phoneNum, emergencyContant) {
+        this.phoneNum = phoneNum;
+        this.emergencyContant = emergencyContant;
         this.fName = fName;
         this.lName = lName;
     }
@@ -22,23 +24,39 @@ var Person = /** @class */ (function () {
     return Person;
 }());
 ;
+var MediacalStaff = /** @class */ (function (_super) {
+    __extends(MediacalStaff, _super);
+    function MediacalStaff(fName, lName, phoneNum, emergencyContant, staffID, position, department) {
+        var _this = _super.call(this, fName, lName, phoneNum, emergencyContant) || this;
+        _this.staffID = staffID;
+        _this.position = position;
+        _this.department = department;
+        return _this;
+    }
+    ;
+    return MediacalStaff;
+}(Person));
 var Patient = /** @class */ (function (_super) {
     __extends(Patient, _super);
-    function Patient(fName, lName, patientID) {
-        var _this = _super.call(this, fName, lName) || this;
+    function Patient(fName, lName, patientID, phoneNum, emergencyContant, medicalHistory) {
+        var _this = _super.call(this, fName, lName, phoneNum, emergencyContant) || this;
         _this.patientID = patientID;
+        _this.medicalHistory = medicalHistory;
         return _this;
     }
     Patient.prototype.patientInfo = function () {
         return "Patient name is: ".concat(this.fName, " ").concat(this.lName, ". and his ID is: ").concat(this.patientID);
     };
     ;
+    Patient.prototype.updateMedic = function (newData) {
+        this.medicalHistory = newData;
+    };
     return Patient;
 }(Person));
 var Doctor = /** @class */ (function (_super) {
     __extends(Doctor, _super);
-    function Doctor(fName, lName, doctorID, specialization) {
-        var _this = _super.call(this, fName, lName) || this;
+    function Doctor(fName, lName, phoneNum, emergencyContant, staffID, position, department, doctorID, specialization) {
+        var _this = _super.call(this, fName, lName, phoneNum, emergencyContant, staffID, position, department) || this;
         _this.doctorID = doctorID;
         _this.specialization = specialization;
         return _this;
@@ -48,27 +66,53 @@ var Doctor = /** @class */ (function (_super) {
     };
     ;
     return Doctor;
-}(Person));
+}(MediacalStaff));
 ;
 var Appointment = /** @class */ (function () {
-    function Appointment(patient, doctor, date, time) {
+    function Appointment(patient, doctor, date, time, status) {
         this.patient = patient;
         this.doctor = doctor;
         this.date = date;
         this.time = time;
+        this.status = status;
     }
     Appointment.prototype.appointmentInfo = function () {
-        return "\n        appoitnment date: ".concat(this.date, " , ").concat(this.time, " .\n        patient name: ").concat(this.patient.fName, " ").concat(this.patient.lName, ". and his ID is: ").concat(this.patient.patientID, "\n        doctor name: ").concat(this.doctor.fName, " ").concat(this.doctor.lName, ". and his ID is: ").concat(this.doctor.doctorID, " specialization - ").concat(this.doctor.specialization);
+        return "\n        appoitnment date: ".concat(this.date, " , ").concat(this.time, " .\n        patient name: ").concat(this.patient.fName, " ").concat(this.patient.lName, ". and his ID is: ").concat(this.patient.patientID, "\n        doctor name: ").concat(this.doctor.fName, " ").concat(this.doctor.lName, ". and his ID is: ").concat(this.doctor.doctorID, " specialization - ").concat(this.doctor.specialization, "\n        status of this appointment: ").concat(this.status);
     };
     ;
+    Appointment.prototype.statusSet = function (word) {
+        if (word === "available" || word === "not available" || word === "cancelled") {
+            this.status = word;
+        }
+        else {
+            console.log("not a valid input");
+        }
+        ;
+    };
+    ;
+    Appointment.prototype.getStatus = function () {
+        return this.status;
+    };
     return Appointment;
 }());
 ;
+var MedicalRecord = /** @class */ (function () {
+    function MedicalRecord(patient, doctor, diagnosis, presciption) {
+        this.patient = patient;
+        this.doctor = doctor;
+        this.diagnosis = diagnosis;
+        this.presciption = presciption;
+    }
+    ;
+    return MedicalRecord;
+}());
+;
 var Hospital = /** @class */ (function () {
-    function Hospital(patientsArr, doctorsArr, appointmentsArr) {
+    function Hospital(patientsArr, doctorsArr, appointmentsArr, medicalRecArr) {
         this.patientsArr = patientsArr;
         this.doctorsArr = doctorsArr;
         this.appointmentsArr = appointmentsArr;
+        this.medicalRecArr = medicalRecArr;
     }
     ;
     Hospital.prototype.addPatient = function (patient) {
@@ -81,6 +125,10 @@ var Hospital = /** @class */ (function () {
     ;
     Hospital.prototype.addDoctor = function (doctor) {
         this.doctorsArr.push(doctor);
+    };
+    ;
+    Hospital.prototype.addMedicalRec = function (medicalRec) {
+        this.medicalRecArr.push(medicalRec);
     };
     ;
     Hospital.prototype.showAppointments = function () {
@@ -103,6 +151,22 @@ var Hospital = /** @class */ (function () {
                 console.log(element.appointmentInfo());
             }
             ;
+        });
+    };
+    ;
+    Hospital.prototype.getMedicalRec = function (pID) {
+        this.medicalRecArr.forEach(function (element) {
+            if (element.patient.patientID === pID) {
+                console.log("Patient: ".concat(element.patient.fName, " ").concat(element.patient.lName, " diagnosis: ").concat(element.diagnosis));
+            }
+        });
+    };
+    ;
+    Hospital.prototype.getDocSchedule = function (docID) {
+        this.appointmentsArr.forEach(function (element) {
+            if (element.doctor.doctorID === docID && element.getStatus() === "available") {
+                console.log("Doctor ".concat(element.doctor.lName + " " + element.doctor.lName, " available at this date ").concat(element.date));
+            }
         });
     };
     ;
--- hosp.ts
+++ hosp.ts
@@ -1,32 +1,59 @@
 class Person{
     fName:string;
     lName:string;
+    phoneNum:number;
+    emergencyContant:string;
 
-    constructor(fName:string,lName:string){
+
+    constructor(fName:string,lName:string,phoneNum:number,emergencyContant:string){
+        this.phoneNum = phoneNum;
+        this.emergencyContant =emergencyContant;
         this.fName = fName;
         this.lName = lName;
     };
 };
 
+class MediacalStaff extends Person{
+    staffID:number;
+    position:string;
+    department:string;
+
+    constructor(fName:string,lName:string,phoneNum:number,emergencyContant:string,staffID:number,position:string,department:string){
+        super(fName,lName,phoneNum,emergencyContant);
+        this.staffID = staffID;
+        this.position = position;
+        this.department = department;
+    };
+    
+}
+
 class Patient extends Person{
     patientID:number;
+    medicalHistory: string[];
 
-    constructor(fName:string,lName:string,patientID:number){
-        super(fName,lName);
+
+    constructor(fName:string,lName:string,patientID:number,phoneNum:number,emergencyContant:string,medicalHistory: string[]){
+        super(fName,lName,phoneNum,emergencyContant);
         this.patientID = patientID;
+        this.medicalHistory = medicalHistory;
+
     }
 
     patientInfo():string{
         return `Patient name is: ${this.fName} ${this.lName}. and his ID is: ${this.patientID}`;
     };
+
+    updateMedic(newData:string[]){
+        this.medicalHistory = newData;
+    }
 }
 
-class Doctor extends Person{
+class Doctor extends MediacalStaff{
     doctorID:number;
     specialization: string;
 
-    constructor(fName:string,lName:string,doctorID:number,specialization: string){
-        super(fName,lName);
+    constructor(fName:string,lName:string,phoneNum:number,emergencyContant:string,staffID:number,position:string,department:string,doctorID:number,specialization: string){
+        super(fName,lName,phoneNum,emergencyContant,staffID,position,department);
         this.doctorID =doctorID;
         this.specialization = specialization;
     }
@@ -42,36 +69,69 @@ class Appointment{
     doctor:Doctor;
     date:string;
     time:string;
+    private status:string;
 
-    constructor(patient:Patient,doctor:Doctor,date:string,time:string){
+    constructor(patient:Patient,doctor:Doctor,date:string,time:string,status:string){
         this.patient = patient;
         this.doctor = doctor;
         this.date = date;
         this.time = time;
+        this.status = status;
     }
 
     appointmentInfo():string{
         return `
         appoitnment date: ${this.date} , ${this.time} .
         patient name: ${this.patient.fName} ${this.patient.lName}. and his ID is: ${this.patient.patientID}
-        doctor name: ${this.doctor.fName} ${this.doctor.lName}. and his ID is: ${this.doctor.doctorID} specialization - ${this.doctor.specialization}`
+        doctor name: ${this.doctor.fName} ${this.doctor.lName}. and his ID is: ${this.doctor.doctorID} specialization - ${this.doctor.specialization}
+        status of this appointment: ${this.status}`
+    };
+
+    statusSet(word:string){
+        if (word === "available" || word === "not available" || word === "cancelled" ){
+            this.status = word;
+        }else{
+            console.log("not a valid input");
+        };
+    };
+
+    getStatus():string{
+        return this.status;
+    }
+};
+
+class MedicalRecord{
+    patient:Patient;
+    doctor:Doctor;
+    diagnosis:string;
+    presciption:string;
+
+    constructor(patient:Patient,doctor:Doctor,diagnosis:string,presciption:string){
+        this.patient = patient;
+        this.doctor = doctor;
+        this.diagnosis = diagnosis;
+        this.presciption = presciption;
     };
 };
 
 class Hospital{
-    patientsArr:Patient[];
+    private patientsArr:Patient[];
     doctorsArr:Doctor[];
     appointmentsArr:Appointment[];
+    medicalRecArr:MedicalRecord[];
 
-    constructor(patientsArr:Patient[],doctorsArr:Doctor[],appointmentsArr:Appointment[]){
+    constructor(patientsArr:Patient[],doctorsArr:Doctor[],appointmentsArr:Appointment[],medicalRecArr:MedicalRecord[]){
         this.patientsArr = patientsArr;
         this.doctorsArr = doctorsArr;
         this.appointmentsArr = appointmentsArr;
+        this.medicalRecArr = medicalRecArr;
     };
 
     addPatient(patient:Patient){
         this.patientsArr.push(patient)
     };
+
+
 
     addAppointment(appointment:Appointment){
         this.appointmentsArr.push(appointment)
@@ -79,6 +139,10 @@ class Hospital{
 
     addDoctor(doctor:Doctor){
         this.doctorsArr.push(doctor);
+    };
+
+    addMedicalRec(medicalRec:MedicalRecord){
+        this.medicalRecArr.push(medicalRec);
     };
 
     showAppointments(){
@@ -103,6 +167,21 @@ class Hospital{
         });
     };
 
-    
+    getMedicalRec(pID:number){
+        this.medicalRecArr.forEach(element => {
+            if(element.patient.patientID === pID){
+                console.log(`Patient: ${element.patient.fName} ${element.patient.lName} diagnosis: ${element.diagnosis}`);
+            }
+        });
+    };
 
+    getDocSchedule(docID:number){        
+        this.appointmentsArr.forEach(element => {
+            if(element.doctor.doctorID === docID && element.getStatus() === "available"){
+                console.log(`Doctor ${element.doctor.lName + " " +element.doctor.lName } available at this date ${element.date}`);}
+        });
+    };
 }
+
+
+
